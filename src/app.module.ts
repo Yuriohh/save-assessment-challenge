632ba6f0b60a7a03937df187f4d8234ab49926cc
Yuriohh <yuri.y.u.r.i@hotmail.com>
2022-07-07 00:40:10 -0300

    Task: add module controller and service to prova

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProvaModule } from './prova/prova.module';
 
 @Module({
-  imports: [],
+  imports: [ProvaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/prova/prova.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('prova')
+export class ProvaController {}
