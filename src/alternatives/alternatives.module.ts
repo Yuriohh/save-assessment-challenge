9c4dfea6900973d683f243383283c126e3cc0c1f
Yuriohh <yuri.y.u.r.i@hotmail.com>
2022-07-23 20:12:19 -0300

    Fix: typeorm data-source fix configs

--- src/alternatives/alternatives.module.ts
+++ src/alternatives/alternatives.module.ts
@@ -1,13 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AssessmentService } from 'src/assessment/assessment.service';
-import { Questions } from 'src/questions/questions.entity';
 import { QuestionsModule } from 'src/questions/questions.module';
-import { QuestionsService } from 'src/questions/questions.service';
 import { AlternativesController } from './alternatives.controller';
-
-import { Alternatives } from './alternatives.entity';
 import { AlternativesRepository } from './alternatives.repository';
 import { AlternativesService } from './alternatives.service';
 
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,16 +1,15 @@
-//import { ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
-//const configService = new ConfigService();
+const configService = new ConfigService();
 
 const AppDataSource = {
   type: 'mysql',
-  host: 'localhost', //configService.get<string>('DB_HOST'), //process.env.DB_HOST,
-  port: 3307, //configService.get<number>('DB_PORT'), //+process.env.DB_PORT,
-  username: 'root', //configService.get<string>('DB_USER'), //process.env.DB_USER,
-  password: 'admin', //configService.get<string>('DB_PASSWORD'), //process.env.DB_PASSWORD,
-  database: 'save-test', //configService.get<string>('DB_NAME'), //process.env.DB_NAME,
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
   entities: [`src/**/*.entity.ts`],
-  //autoLoadEntities: true,
   synchronize: false,
   migrationsTableName: 'migrations',
   migrations: ['src/migrations/*.ts'],
